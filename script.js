4f3351c578ef1f6f44ea76db9a8aef37263f767e
AnnaSharv <79162492+AnnaSharv@users.noreply.github.com>
2021-12-10 13:44:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const scrollContent = document.querySelector('.my-modal-body')
- scrollContent.addEventListener("scroll",  ()=> {
-   const x = scrollContent.getBoundingClientRect();
-   const y = scrollContent.offsetHeight;
-   console.log(y,x);
- })
+// const scrollContent = document.querySelector('.my-modal-body')
+//  scrollContent.addEventListener("scroll",  ()=> {
+//    const x = scrollContent.getBoundingClientRect();
+//    const y = scrollContent.offsetHeight;
+//    console.log(y,x);
+//  })
 
